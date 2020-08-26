f593695e4d71608c0578202079996f11331edcf9
marumaidana <maruu.mariela@gmail.com>
2020-08-26 20:51:41 -0300

    Ejercitacion-Playlist

---
+++ index.js
@@ -0,0 +1,15 @@
+const cancion1 = prompt("Decime una cancion")
+const cancion2 = prompt("Decime una cancion")
+const cancion3 = prompt("Decime una cancion")
+const cancion4 = prompt("Decime una cancion")
+const cancion5 = prompt("Decime una cancion")
+
+const lis = document.getElementsByTagName("li")
+
+
+console.log(lis)
+lis[0].textContent = cancion1
+lis[1].textContent = cancion2
+lis[2].textContent = cancion3
+lis[3].textContent = cancion4
+lis[4].textContent = cancion5
